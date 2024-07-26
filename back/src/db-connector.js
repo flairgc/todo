70badb51a54207a3e96e1e24a08f1153c9e3f318
Фисенко Андрей Юрьевич <afisenko@phoenixit.ru>
2024-07-26 19:17:32 +0300

    BACK: add test DB connection

--- back/src/db-connector.js
+++ back/src/db-connector.js
@@ -9,6 +9,22 @@ async function dbConnector (fastify, options) {
   fastify.register(fastifyPostgres, {
     connectionString: process.env.APP_DB_CONNECTION_STRING
   })
+  // test connection
+  fastify.after(async (err) => {
+    if (err) {
+      fastify.log.error(err)
+      return
+    }
+    try {
+      await fastify.pg.query(
+        'SELECT 1',
+      )
+      fastify.log.info('Connection to DB is successful')
+    } catch (err) {
+      console.error('Connection to DB failed', err);
+      fastify.log.error('Connection to DB failed', err)
+    }
+  })
 }
 
 // Wrapping a plugin function with fastify-plugin exposes the decorators
